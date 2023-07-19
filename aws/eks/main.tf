9ceacd70e8e8c5e508112a3238140790e28b9978
Boris IZA <devops@123cloud.cl>
2023-07-19 11:11:17 -0400

    add kubernetes provider resources

--- aws/eks/main.tf
+++ aws/eks/main.tf
@@ -135,3 +135,20 @@ resource "kubernetes_config_map" "aws_auth" {
     aws_eks_cluster.cluster,
   ]
 }
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks_fargate.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks_fargate.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.9"
+}
+
