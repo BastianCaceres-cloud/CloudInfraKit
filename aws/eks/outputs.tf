cfd16e580a660a8a366ebb439de1c76293d872ca
Boris IZA <devops@123cloud.cl>
2023-07-20 11:21:23 -0400

    new outputs module eks

--- aws/eks/outputs.tf
+++ aws/eks/outputs.tf
@@ -23,3 +23,13 @@ output "cluster_endpoint" {
   description = "The endpoint for your EKS Kubernetes API."
   value       = aws_eks_cluster.cluster.endpoint
 }
+
+output "cluster_role_arn" {
+  description = "The ARN of the EKS cluster role"
+  value       = aws_iam_role.cluster.arn
+}
+
+output "fargate_pod_execution_role_arn" {
+  description = "The ARN of the Fargate pod execution role"
+  value       = aws_iam_role.fargate_pod_execution_role.arn
+}
