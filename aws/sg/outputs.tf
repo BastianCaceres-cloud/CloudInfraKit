c8255640a76fd5c2f6ddbad4a5dff755900edab5
Boris IZA <devops@123cloud.cl>
2023-07-26 12:27:58 -0400

    add new module

--- aws/sg/outputs.tf
+++ aws/sg/outputs.tf
@@ -1,9 +1,49 @@
-output "queue_url" {
-  description = "The URL of the SQS Queue"
-  value       = aws_sqs_queue.terraform_queue.id
+output "id" {
+  description = "IDs of the created security groups"
+  value       = values(aws_security_group.this)[*].id
 }
 
-output "queue_arn" {
-  description = "The ARN of the SQS Queue"
-  value       = aws_sqs_queue.terraform_queue.arn
+output "arn" {
+  description = "ARNs of the created security groups"
+  value       = values(aws_security_group.this)[*].arn
 }
+
+output "name" {
+  description = "Names of the created security groups"
+  value       = values(aws_security_group.this)[*].name
+}
+
+output "description" {
+  description = "Descriptions of the created security groups"
+  value       = values(aws_security_group.this)[*].description
+}
+
+output "ingress_rules" {
+  description = "List of ingress rules for the created security groups"
+  value       = values(aws_security_group.this)[*].ingress
+}
+
+output "egress_rules" {
+  description = "List of egress rules for the created security groups"
+  value       = values(aws_security_group.this)[*].egress
+}
+
+output "security_groups" {
+  description = "Map describing the created security groups"
+  value = {
+    for name, sg in aws_security_group.this :
+    name => {
+      id          = sg.id
+      vpc_id      = sg.vpc_id
+      name        = sg.name
+      description = sg.description
+      ingress     = sg.ingress
+      egress      = sg.egress
+      tags        = sg.tags
+    }
+  }
+}
+
+
+
+
