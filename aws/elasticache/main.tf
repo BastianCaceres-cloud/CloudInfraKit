d0d5da770a6b1256234dbd4d17ad9786f9405c30
Boris IZA <devops@123cloud.cl>
2023-07-26 14:30:03 -0400

    fix duplicated resoruce elasticache

--- aws/elasticache/main.tf
+++ aws/elasticache/main.tf
@@ -5,16 +5,7 @@ resource "aws_elasticache_subnet_group" "example" {
   subnet_ids = var.subnet_ids
 }
 
-resource "aws_elasticache_cluster" "example" {
-  cluster_id           = var.cluster_id
-  node_type            = var.node_type
-  num_cache_nodes      = var.num_cache_nodes
-  engine               = "redis"
-  engine_version       = var.engine_version
-  subnet_group_name    = aws_elasticache_subnet_group.example.name
-  security_group_ids   = var.security_group_ids
-  parameter_group_name = "default.redis7.0"
-}
+
 
 # Resources
 
