9985cff0a346524f94ef148a11421e1872388377
Boris IZA <devops@123cloud.cl>
2023-07-26 14:09:56 -0400

    fix elasticache redis module

--- aws/elasticache/variables.tf
+++ aws/elasticache/variables.tf
@@ -28,17 +28,6 @@ variable "security_group_ids" {
   type        = list(string)
 }
 
-resource "aws_elasticache_cluster" "example" {
-  cluster_id           = var.cluster_id
-  node_type            = var.node_type
-  num_cache_nodes      = var.num_cache_nodes
-  engine               = "redis"
-  engine_version       = var.engine_version
-  subnet_group_name    = var.subnet_group_name
-  security_group_ids   = var.security_group_ids
-  parameter_group_name = "default.redis5.0"
-}
-
 variable "subnet_ids" {
   description = "List of subnet IDs to associate with the subnet group"
   type        = list(string)
